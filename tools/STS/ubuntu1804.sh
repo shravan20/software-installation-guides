268a11915dc5ca1f0b845e0af3a91736d29916e7
shravan20 <mrshravankumarb@gmail.com>
2021-04-20 22:45:05 +0530

    :hammer: Added Shell Script
    Added shell script for installation of STS

---
+++ tools/STS/ubuntu1804.sh
@@ -0,0 +1,22 @@
+
+# Please follow the instructions in the README
+
+sudo mv spring-tool-suite-3.9.10.RELEASE-e4.13.0-linux-gtk-x86_64.tar.gz /opt
+cd /opt/
+sudo tar zxvf spring-tool-suite-3.9.10.RELEASE-e4.13.0-linux-gtk-x86_64.tar.gz
+sudo ln -s /opt/sts-bundle/sts-3.9.10.RELEASE/STS /usr/local/bin/sts
+
+
+
+
+
+
+# sudo echo "[Desktop Entry]
+# Name=SpringSource Tool Suite
+# Comment=SpringSource Tool Suite
+# Exec=~/springsource/sts-3.4.0-RELEASE/STS
+# Icon=~/springsource/sts-3.4.0-RELEASE/icon.xpm
+# StartupNotify=true
+# Terminal=false
+# Type=Application
+# Categories=Development;IDE;Java;" >> /usr/share/applications/stsLauncher.desktop
